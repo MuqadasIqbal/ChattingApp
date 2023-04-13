32d2d80d65ca16de4149bc5915af53d4adb365cd
Muqadas Iqbal <muqadasiqbal807@gmail.com>
2023-04-13 22:55:10 +0500

    48Mintues OTP

---
+++ app/src/main/java/com/example/chatsapp/PhoneNumberActivity.java
@@ -0,0 +1,35 @@
+package com.example.chatsapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.chatsapp.databinding.ActivityPhoneNumberBinding;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class PhoneNumberActivity extends AppCompatActivity {
+
+    ActivityPhoneNumberBinding binding;
+    FirebaseAuth auth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityPhoneNumberBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        auth=FirebaseAuth.getInstance();
+        getSupportActionBar().hide();
+
+        binding.phoneBox.requestFocus();
+
+        binding.continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(PhoneNumberActivity.this,OTPActivity.class);
+                intent.putExtra("phoneNumber",binding.phoneBox.getText().toString());
+                startActivity(intent);
+            }
+        });
+    }
+}
